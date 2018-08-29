fa61a38af8a7fcfe91a2a3c24701ff284a9db474
caklovic2 <caklovicka1@gmail.com>
2018-08-29 20:46:41 +0200

    fix

--- QRparallel_experimental.c
+++ QRparallel_experimental.c
@@ -206,7 +206,7 @@ int main(int argc, char* argv[]){
 					if(j==7) printMatrix(&G[70], M, 1);
 
 					if(j == 7)printf("norm[%d] (prije) = %lg\n", j, norm[j]);
-					if(j == 7)printf("norm[%d] = %lg - %lg\n", j, norm[j], (double) (conj(G[k-1+M*j]) * J[k-1] * G[k-1+M*j]));
+					if(j == 7)printf("norm[%d] = %lg - %lg\n", j, norm[j], creal(conj(G[k-1+M*j]) * J[k-1] * G[k-1+M*j]));
 
 					// not a case of catastrophic cancellation
 					if( cabs(norm[j] - conj(G[k-1+M*j]) * J[k-1] * G[k-1+M*j]) > DBL_EPSILON * 100)
