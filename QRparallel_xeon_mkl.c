a9fb19e31af53c0d7b6633bc536998f3884da34b
caklovic2 <caklovicka1@gmail.com>
2018-08-29 19:12:44 +0200

    fix

--- QRparallel_xeon_mkl.c
+++ QRparallel_xeon_mkl.c
@@ -203,7 +203,7 @@ int main(int argc, char* argv[]){
 			#pragma omp parallel for reduction(+:Aik) num_threads( (int)csqrt(nthreads) )
 			for(j = k; j < M; ++j) Aik += conj(G[j+M*i]) * J[j] * G[j+M*k];
 
-			printf("Aik = %lg\n", creal(Aik), cimag(Aik));
+			printf("Aik = %lg + i %lg\n", creal(Aik), cimag(Aik));
 
 			#pragma omp critical
 			if(pivot_lambda < cabs(Aik)){
@@ -224,7 +224,7 @@ int main(int argc, char* argv[]){
 			#pragma omp parallel for reduction(+:Air) num_threads( (int)csqrt(nthreads) )
 			for(j = k; j < M; ++j)	Air += conj(G[j+M*i]) * J[j] * G[j+M*pivot_r];
 
-			printf("Air = %lg\n", creal(Air), cimag(Air));
+			printf("Air = %lg + i %lg\n", creal(Air), cimag(Air));
 
 			if(pivot_sigma < cabs(Air)) pivot_sigma = cabs(Air);
 		}
