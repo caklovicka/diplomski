4bcf7434793f82d973dd6674d5adfccb0a6ca0e5
caklovic2 <caklovicka1@gmail.com>
2018-09-04 19:25:50 +0200

    redukcija fix

--- QRparallel_experimental.c
+++ QRparallel_experimental.c
@@ -871,6 +871,8 @@ int main(int argc, char* argv[]){
 				#pragma omp parallel for num_threads( kth_nonzeros ) private(i)
 				for(i = k; i < k + kth_nonzeros; ++i){
 
+					printf("%lg\n", cimag(G[i + M*k]));
+
 					if( cimag(G[i + M*k]) < EPSILON) continue;
 
 					mkl_set_num_threads_local(mkl_nthreads);
