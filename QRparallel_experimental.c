fccdfdd5ea2a4178984ce6cd984dfce60180b5c3
caklovic2 <caklovicka1@gmail.com>
2018-09-12 00:00:27 +0200

    skroz sve sa Householderima

--- QRparallel_experimental.c
+++ QRparallel_experimental.c
@@ -258,8 +258,6 @@ int main(int argc, char* argv[]){
 							// will be used for column swap k+1 <-> pivot_r when PIVOT_2 begins
 
 		double Akk = (double) norm[k];
-		printf("A%d = %lg\n", k, Akk);
-		goto PIVOT_1;
 
 		if(k == N-1) goto PIVOT_1;
 
