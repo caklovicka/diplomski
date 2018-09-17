c36fa03be5de1a62b2de27522bd2694e7242d134
caklovic2 <caklovicka1@gmail.com>
2018-09-17 23:56:28 +0200

    matricni korijen

--- JHouseholder.c
+++ JHouseholder.c
@@ -476,6 +476,7 @@ int main(int argc, char* argv[]){
 			printf("detG1 = %lg + i%lg\n", creal(G[k+M*k]*G[k+1+M*(k+1)] - G[k+1+M*k]*G[k+M*(k+1)]), cimag(G[k+M*k]*G[k+1+M*(k+1)] - G[k+1+M*k]*G[k+M*(k+1)]));
 			printf("K = \n");
 			printMatrix(K, 2, 2);
+			printf("Jk = %d, Jk+1 = %d\n", J[k], J[k+1]);
 
 			printf("T^2 = \n");
 			C[0] = T[0]*T[0] + T[2]*T[1];
