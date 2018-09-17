5fdcfb638456ed3347bd3b01c508fa8df40ddcb9
caklovic2 <caklovicka1@gmail.com>
2018-09-17 19:42:21 +0200

    matricni korijen

--- JHouseholder.c
+++ JHouseholder.c
@@ -502,7 +502,7 @@ int main(int argc, char* argv[]){
 
 
 
-		/*printf("F1* J F1 = \n");
+		printf("F1* J F1 = \n");
 		C[0] = J[k]*T[0]*conj(T[0]) + J[k+1]*T[1]*conj(T[1]);
 		C[1] = J[k]*T[0]*conj(T[2]) + J[k+1]*T[1]*conj(T[3]);
 		C[2] = J[k]*T[2]*conj(T[0]) + J[k+1]*T[3]*conj(T[1]);
@@ -515,7 +515,7 @@ int main(int argc, char* argv[]){
 		C[2] = Akr;
 		C[3] = Arr;
 		printMatrix(C, 2, 2);
-		*/
+		
 
 
 
