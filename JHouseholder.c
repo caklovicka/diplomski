61f0eca149cda1a8e27d54262c452a8dd99db69f
caklovic2 <caklovicka1@gmail.com>
2018-09-18 22:24:17 +0200

    pa jel toto vise

--- JHouseholder.c
+++ JHouseholder.c
@@ -401,7 +401,7 @@ int main(int argc, char* argv[]){
 
 			double xJx = conj(G[k+M*k]) * J[k] * G[k+M*k] + conj(G[i+M*k]) * J[k+1] * G[i+M*k];
 			double yJy = conj(G[k+M*(k+1)]) * J[k] * G[k+M*(k+1)] + conj(G[i+M*(k+1)]) * J[k+1] * G[i+M*(k+1)];
-			double xJy = conj(G[k+M*k]) * J[k] * G[k+M*(k+1)] + conj(G[i+M*k]) * J[k+1] * G[i+M*(k+1)];
+			double complex xJy = conj(G[k+M*k]) * J[k] * G[k+M*(k+1)] + conj(G[i+M*k]) * J[k+1] * G[i+M*(k+1)];
 			double trace = K[0] * xJx + K[3] * yJy + 2 * creal( K[2] * xJy );
 
 			double det = -cabs(detG1) * cabs(detG1) / detA;
