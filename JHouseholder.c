4731759ad7408640f5ffdba0da94110ced19255f
caklovic2 <caklovicka1@gmail.com>
2018-09-18 23:19:18 +0200

    pa jel toto vise

--- JHouseholder.c
+++ JHouseholder.c
@@ -594,10 +594,10 @@ int main(int argc, char* argv[]){
 
 
 		// fill first two columns of G
-		//G[k+M*k] = T[0];
-		//G[k+1+M*k] = T[1];
-		//G[k+M*(k+1)] = T[2];
-		//G[k+1+M*(k+1)] = T[3];
+		G[k+M*k] = T[0];
+		G[k+1+M*k] = T[1];
+		G[k+M*(k+1)] = T[2];
+		G[k+1+M*(k+1)] = T[3];
 
 
 		// compute K*JK, first we need T = JK
@@ -608,10 +608,10 @@ int main(int argc, char* argv[]){
 		for(i = k; i < M; ++i){
 			T[i] = J[i] * K[i];
 			T[i+M] = J[i] * K[i+M];
-			//if( i >= k+2 ){
-			//	G[i+M*k] = 0;
-			//	G[i+M*(k+1)] = 0;
-			//}
+			if( i >= k+2 ){
+				G[i+M*k] = 0;
+				G[i+M*(k+1)] = 0;
+			}
 		}
 
 		// compute K*T, where T = JK
@@ -664,26 +664,26 @@ int main(int argc, char* argv[]){
 		// K = W (Mk x 2 matrix)
 		// C = (W*JW)^+ (2x2 matrix)
 		// T = JK
-		//#pragma omp parallel num_threads( nthreads )
-		//{
+		#pragma omp parallel num_threads( nthreads )
+		{
 			//#pragma omp for nowait
-			for(j = k; j < N; j += 1){
+			for(j = k+2; j < N; j += 2){
 
-				//mkl_set_num_threads_local(mkl_nthreads);
-				//double complex *CC = (double complex*) mkl_malloc(4*sizeof(double complex), 64);
+				mkl_set_num_threads_local(mkl_nthreads);
+				double complex *CC = (double complex*) mkl_malloc(4*sizeof(double complex), 64);
 
 				// case when we have 2 columns of G to work with
-				if(0){//j != N-1
+				if(j != N-1){//j != N-1
 
 					// CC  = T*G
 					alpha = 1;
 					beta = 0;
-					zgemm(&trans, &nontrans, &n, &n, &Mk, &alpha, &T[k], &M, &G[k+M*j], &M, &beta, C, &n);
+					zgemm(&trans, &nontrans, &n, &n, &Mk, &alpha, &T[k], &M, &G[k+M*j], &M, &beta, CC, &n);
 
 					// G = G - 2E CC
 					alpha = -2;
 					beta = 1;
-					zgemm(&nontrans, &nontrans, &Mk, &n, &n, &alpha, &E[k], &M, C, &n, &beta, &G[k+M*j], &M);
+					zgemm(&nontrans, &nontrans, &Mk, &n, &n, &alpha, &E[k], &M, CC, &n, &beta, &G[k+M*j], &M);
 				}
 
 				// case when we are in the last column
@@ -693,17 +693,17 @@ int main(int argc, char* argv[]){
 					alpha = 1;
 					beta = 0;
 					inc = 1;
-					zgemv(&trans, &Mk, &n, &alpha, &T[k], &M, &G[k+M*j], &inc, &beta, C, &inc);
+					zgemv(&trans, &Mk, &n, &alpha, &T[k], &M, &G[k+M*j], &inc, &beta, CC, &inc);
 
 					// g = g - 2E CC
 					alpha = -2;
 					beta = 1;
-					zgemv(&nontrans, &Mk, &n, &alpha, &E[k], &M, C, &inc, &beta, &G[k+M*j], &inc);
+					zgemv(&nontrans, &Mk, &n, &alpha, &E[k], &M, CC, &inc, &beta, &G[k+M*j], &inc);
 				}
 
-				//mkl_free(CC);
+				mkl_free(CC);
 			}
-		//}
+		}
 		mkl_set_num_threads_local(0);
 
 		k = k+1;
