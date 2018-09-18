45a3bc798644d1f1a0fee4cee4b5cc84da05c3ac
caklovic2 <caklovicka1@gmail.com>
2018-09-19 00:14:19 +0200

    pa jel toto vise

--- JHouseholder.c
+++ JHouseholder.c
@@ -670,13 +670,13 @@ int main(int argc, char* argv[]){
 		//#pragma omp parallel num_threads( nthreads )
 		//{
 			//#pragma omp for nowait
-			for(j = k+2; j < N; j += 1){
+			for(j = k+2; j < N; j += 2){
 
 				//mkl_set_num_threads_local(mkl_nthreads);
 				//double complex *CC = (double complex*) mkl_malloc(4*sizeof(double complex), 64);
 
 				// case when we have 2 columns of G to work with
-				if(0){//j != N-1
+				if(j != N-1){//j != N-1
 
 					// CC  = T*G
 					alpha = 1;
