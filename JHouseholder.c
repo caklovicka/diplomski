8de5cdaf8ba979ee86f013ad3671a3d2aea27bc9
caklovic2 <caklovicka1@gmail.com>
2018-09-20 13:48:35 +0200

    popravak sa poljem c

--- JHouseholder.c
+++ JHouseholder.c
@@ -597,7 +597,7 @@ int main(int argc, char* argv[]){
 		//{
 			//mkl_set_num_threads_local( mkl_nthreads );
 
-			#pragma omp parallel for num_threads( nthreads )
+			#pragma omp parallel for num_threads( nthreads ) shared(G, T, E, K)
 			for(j = k+2; j < N; ++j){
 
 				mkl_set_num_threads_local(1);
