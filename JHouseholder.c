ced56b167a7f9b99be0da890bdce9b0b624682c3
caklovic2 <caklovicka1@gmail.com>
2018-09-20 15:11:03 +0200

    popravak sa poljem c

--- JHouseholder.c
+++ JHouseholder.c
@@ -608,7 +608,7 @@ int main(int argc, char* argv[]){
 
 				//mkl_set_num_threads_local(1);
 
-				/*double complex a, b;
+				double complex a, b;
 				inc = 1;
 				Mk = M - k;
 				// a = T1* g
@@ -617,18 +617,18 @@ int main(int argc, char* argv[]){
 				zdotc(&b, &Mk, &T[k+M], &inc, &G[k+M*j], &inc);
 				//g = g - 2E [a b]^T
 				for(i = k; i < M; ++i) G[i+M*j] -= 2 * (E[i]*a + E[i+M]*b);
-				*/
+				
 				// case when we are in the last column
 
 				// K = T*g
-				alpha = 1;
+				/*alpha = 1;
 				beta = 0;
 				zgemv(&trans, &Mk, &n, &alpha, &T[k], &M, &G[k+M*j], &inc, &beta, &K[2*j], &inc);
 
 				// g = g - 2E K
 				alpha = -2;
 				beta = 1;
-				zgemv(&nontrans, &Mk, &n, &alpha, &E[k], &M, &K[2*j], &inc, &beta, &G[k+M*j], &inc);
+				zgemv(&nontrans, &Mk, &n, &alpha, &E[k], &M, &K[2*j], &inc, &beta, &G[k+M*j], &inc);*/
 
 				// case when we have 2 columns of G to work with
 				/*if(0){//j != N-1
