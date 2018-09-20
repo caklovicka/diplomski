68ffedc22e975fe322bbc6a84713c0270ea6db98
caklovic2 <caklovicka1@gmail.com>
2018-09-20 14:11:18 +0200

    popravak sa poljem c

--- JHouseholder.c
+++ JHouseholder.c
@@ -41,7 +41,7 @@ void printMatrix(double complex *G, int M, int N){
 	int i, j;
 	for( i = 0; i < M; ++i ){
 		for( j = 0; j < N; ++j ){
-			printf("%7.2g + i%7.2g  ", creal(G[i+M*j]), cimag(G[i+M*j]));
+			printf("%15.10g + i%15.10g  ", creal(G[i+M*j]), cimag(G[i+M*j]));
 		}
 		printf("\n");
 	}
