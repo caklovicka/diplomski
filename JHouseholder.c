ce8988a4dc2714efe62156a8bbdfca55a7f12c19
caklovic2 <caklovicka1@gmail.com>
2018-09-20 13:28:36 +0200

    popravak sa poljem c

--- JHouseholder.c
+++ JHouseholder.c
@@ -91,6 +91,7 @@ int main(int argc, char* argv[]){
 	double complex *E = (double complex*) mkl_malloc(2*M*sizeof(double complex), 64);	// temporary matrix
 	int *ipiv = (int*) mkl_malloc(4*sizeof(int), 64);
 	double complex *work = (double complex*) mkl_malloc(4*sizeof(double complex), 64);	// temporary matrix
+	double complex c[2];
 
 
 	// check if files are opened
@@ -593,14 +594,14 @@ int main(int argc, char* argv[]){
 		// E = K(K*JK)^+
 		// T = JK
 		double ss = omp_get_wtime();
-		#pragma omp parallel num_threads( nthreads )
+		#pragma omp parallel num_threads( nthreads ) shared(G, T, E) private(c)
 		{
 			mkl_set_num_threads_local( mkl_nthreads );
 
 			#pragma omp for nowait
 			for(j = k+2; j < N; ++j){
 
-				double complex a, b;
+				/*double complex a, b;
 				inc = 1;
 				Mk = M - k;
 				// a = T1* g
@@ -609,19 +610,19 @@ int main(int argc, char* argv[]){
 				zdotc(&b, &Mk, &T[k+M], &inc, &G[k+M*j], &inc);
 				//g = g - 2E [a b]^T
 				for(i = k; i < M; ++i) G[i+M*j] -= 2 * (E[i]*a + E[i+M]*b);
-
+				*/
 				// case when we are in the last column
 					
-				// CC = T*g
-				/*alpha = 1;
+				// c = T*g
+				alpha = 1;
 				beta = 0;
 				inc = 1;
-				zgemv(&trans, &Mk, &n, &alpha, &T[k], &M, &G[k+M*j], &inc, &beta, CC, &inc);
+				zgemv(&trans, &Mk, &n, &alpha, &T[k], &M, &G[k+M*j], &inc, &beta, c, &inc);
 
-				// g = g - 2E CC
+				// g = g - 2E c
 				alpha = -2;
 				beta = 1;
-				zgemv(&nontrans, &Mk, &n, &alpha, &E[k], &M, CC, &inc, &beta, &G[k+M*j], &inc);
+				zgemv(&nontrans, &Mk, &n, &alpha, &E[k], &M, c, &inc, &beta, &G[k+M*j], &inc);
 
 				// case when we have 2 columns of G to work with
 				/*if(0){//j != N-1
