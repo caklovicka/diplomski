ee0a66f43bd3a4764b2b189e1f30b0636f7a733e
caklovic2 <caklovicka1@gmail.com>
2018-09-23 23:45:59 +0200

    printanje gresaka

--- JHouseholder.c
+++ JHouseholder.c
@@ -410,32 +410,6 @@ int main(int argc, char* argv[]){
 		K[3] = creal(K[3]);
 
 
-		// set J[k] = 1
-
-		int idx = k;
-		while(J[idx] == -1) ++idx;
-
-		// swap rows
-		if( idx != k ){
-
-			double dtemp = J[idx];
-			J[idx] = J[k];
-			J[k] = dtemp;
-
-			// update Prow
-			long int itemp = Prow[idx];
-			Prow[idx] = Prow[k];
-			Prow[k] = itemp;
-
-			// swap rows in G 
-			int Nk = N - k;
-			mkl_nthreads = Nk/D > mkl_get_max_threads() ? Nk/D : mkl_get_max_threads();
-			if(Nk/D == 0) mkl_nthreads = 1;
-			mkl_set_num_threads(mkl_nthreads);
-			zswap(&Nk, &G[k + M*k], &M, &G[idx + M*k], &M);
-		}
-
-
 		// find pivot G1
 
 		nthreads = (M-k-1)/D > omp_get_max_threads() ? (M-k-1)/D : omp_get_max_threads();
