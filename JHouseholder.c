31ef49bb6655c8fed08db27b261a83053f0e6891
caklovic2 <caklovicka1@gmail.com>
2018-09-23 21:34:11 +0200

    printanje gresaka

--- JHouseholder.c
+++ JHouseholder.c
@@ -444,8 +444,7 @@ int main(int argc, char* argv[]){
 			C[3] = G[i+M*(k+1)];
 			zgesdd_(&jobz, &n, &n, C, &n, s, NULL, &n, NULL, &n, work, &lwork, rwork, ipiv, &info);
 			if(info) printf("SVD did not converge... Proceeding...\n");
-			printf("cond = %lg\n", s[0]/s[1]);
-		
+			
 			// condition that a sqrt exists
 			// see: https://www.maa.org/sites/default/files/pdf/cms_upload/Square_Roots-Sullivan13884.pdf
 			if( trace + 2 * creal(csqrt(det)) >= 0 && cabs(s[0]/s[1]) < min_svd){
@@ -457,7 +456,6 @@ int main(int argc, char* argv[]){
 
 		if(idx == -1){
 			printf("No more altering signs in J or no such G1 for finding a sqrt(K^2) ... (in pivot 2, k = %d) ... Exiting\n", k);
-			printJ(&J[k], M-k);
 			break;
 		}
 
