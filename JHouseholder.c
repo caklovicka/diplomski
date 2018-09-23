20cbafaf16849d1cc0b7ad078eaaf116017a1f66
caklovic2 <caklovicka1@gmail.com>
2018-09-23 16:10:26 +0200

    printanje gresaka

--- JHouseholder.c
+++ JHouseholder.c
@@ -534,6 +534,7 @@ int main(int argc, char* argv[]){
 		E[2] = T[0]*T[2] + T[2]*T[3] - K[2];
 		E[3] = T[1]*T[2] + T[3]*T[3] - K[3]; 
 		sqrt_err = dznrm2(&m, E, &inc);
+		printf("sqrt_err = %lg\n", sqrt_err);
 		mkl_set_num_threads(1);
 		while(sqrt_err > sqrt_eps){
 
