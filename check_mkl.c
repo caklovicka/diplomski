f761dc473dea605519bde1b560104a588ee2fc74
caklovic2 <caklovicka1@gmail.com>
2018-10-21 22:14:21 +0200

    with right vectors

--- check_mkl.c
+++ check_mkl.c
@@ -241,7 +241,7 @@ int main(int argc, char* argv[]){
 		fscanf(svd, "%lg ", &ss);
 		norm_svd += (csqrt(s[i]) - csqrt(ss)) * (csqrt(s[i]) - csqrt(ss));
 		if( creal(cabs(csqrt(s[i]) - csqrt(ss))) > max_abs_svd) max_abs_svd = creal(cabs(csqrt(s[i]) - csqrt(ss)));
-		if( creal(cabs(csqrt(s[i]) - csqrt(ss))/csqrt(ss)) > max_rel_err_svd ) max_rel_err_svd = cabs(csqrt(s[i]) - csqrt(ss))/creal(csqrt(ss));
+		if( creal(cabs(csqrt(s[i]) - csqrt(ss)))/creal(csqrt(ss)) > max_rel_err_svd ) max_rel_err_svd = creal(cabs(csqrt(s[i]) - csqrt(ss)))/creal(csqrt(ss));
 	}
 	printf("norm_2(ss - s) = %.6e, max_abs_svd = %.6e, max_rel_err_svd = %.6e\n", csqrt(norm_svd), max_abs_svd, max_rel_err_svd);
 
