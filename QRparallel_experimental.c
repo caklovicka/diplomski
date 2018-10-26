0cd5f64d75cf24413422fae524a16013cf5114c0
g.caklovic <gacaklo@zam494.zam.kfa-juelich.de>
2018-10-26 11:59:22 +0200

    givensi...

--- QRparallel_experimental.c
+++ QRparallel_experimental.c
@@ -1222,7 +1222,7 @@ int main(int argc, char* argv[]){
 
 		#pragma omp parallel num_threads( nthreads )
 		{
-			mkl_set_num_threads_local(1);
+			mkl_set_num_threads_local(mkl_nthreads);
 
 			#pragma omp for nowait
 			for(j = k+1; j < N; ++j){
